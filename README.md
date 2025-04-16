93a727f6da885df2390ca97235b36562f9121170
fng97 <fng97@icloud.com>
2025-04-16 21:54:58 +0100

    Copies websocket client from github:fng97/adventus
    Copied at commit 7211824 (with some minor modifications).
    
    Roughly all autobahn tests passing aside from section 6 (UTF-8
    validation) and sections 12 and 13 (compression).

---
+++ README.md
@@ -0,0 +1,5 @@
+# Adventus
+
+WebSockets library written in Zig.
+
+To test against the Autobahn test suite, run `zig build test`. You must have docker installed.
---
+++ build.zig
@@ -0,0 +1,27 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const lib = b.addStaticLibrary(.{
+        .name = "websockets",
+        .root_source_file = b.path("src/websockets.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    b.installArtifact(lib);
+
+    const lib_unit_tests = b.addTest(.{
+        .root_source_file = b.path("src/websockets.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
+    run_lib_unit_tests.has_side_effects = true; // so that test runs aren't cached
+
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_lib_unit_tests.step);
+}
