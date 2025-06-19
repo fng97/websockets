73c1bbecdc4766a47f03cfcc9f7d6d19e2dc034b
fng97 <fng97@icloud.com>
2025-06-19 11:27:39 +0100

    Add ZLS check step to "build.zig"

--- build.zig
+++ build.zig
@@ -22,6 +22,14 @@ pub fn build(b: *std.Build) void {
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
     run_lib_unit_tests.has_side_effects = true; // so that test runs aren't cached
 
+    const lib_unit_tests_check = b.addTest(.{
+        .root_source_file = b.path("src/websockets.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    const check = b.step("check", "Check: See https://zigtools.org/zls/guides/build-on-save/");
+    check.dependOn(&lib_unit_tests_check.step);
+
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_lib_unit_tests.step);
 }
